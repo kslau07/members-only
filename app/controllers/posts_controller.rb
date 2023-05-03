08cfd43773870a2a1d4b0fc6fb18d0d8edde571b
kslau07 <abluecar@gmail.com>
2023-05-03 18:56:10 -0400

    feat: user and post are associated
    error messages show up in posts/new
    add dependent: :destroy to User model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # def create
+  #   @current_user = current_user
+  #   @post = @current_user.posts.create(post_params)
+
+  #   if @post.save
+  #     flash[:notice] = 'A new post was created!'
+  #     redirect_to posts_path
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end

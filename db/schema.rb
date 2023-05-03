5303aa65f77c08eecb6d02f4c6d88ac6c7f4f175
kslau07 <abluecar@gmail.com>
2023-05-03 19:36:16 -0400

    feat: index shows user_id and secret

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_222735) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_233336) do
   create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
